561ba85a9b535e8e3cb114bbb414c72f75586b54
whskee <chinmayabidkar@hotmail.com>
2024-02-27 00:05:03 -0700

    Init

--- parser.cc
+++ parser.cc
@@ -9,59 +9,10 @@ void SymbolTable::addSymbol(Symbol* symbol) {
     this->symbols.push_back(symbol);
 }
 
-void Parser::addSymbols(set<string> symbols, string scope, string visibility) {
-    for (set<string>::iterator it = symbols.begin(); it != symbols.end(); it++) {
-        Symbol* symbol = new Symbol();
-        symbol->name = *it;
-        symbol->scope = scope;
-        symbol->visibility = visibility;
-        symbolTable.addSymbol(symbol);
-    }
-}
-
-void SymbolTable::deleteSymbol(Symbol* symbol) {
-    vector<Symbol*>::iterator it = std::find(symbols.begin(), symbols.end(), symbol);
-    symbols.erase(it);
-}
-
-void SymbolTable::deleteSymbolsFromScope(string scope) {
-    symbols.rbegin();
-    for (auto it = symbols.rbegin(); it != symbols.rend(); it++) {
-        if ((*it)->scope == scope) {
-            deleteSymbol(*it);
-        } else {
-            return;
-        }
-    }
-}
-
-int SymbolTable::indexOf(string symbolName) {
-    // auto it = std::find(v.begin(), v.end(), K);
-    // // If element was found
-    // if (it != v.end()) {
-    //     int index = it - v.begin();
-    //     return index;
-    // } else {
-    //     // If element was not found
-    //     return -1;
-    // }
-    int index = 0;
-    for (vector<Symbol*>::iterator it = symbols.begin(); it != symbols.end(); it++) {
-        if ((*it)->name == symbolName) {
-            return index;
-        }
-        index++;
-    }
-    return -1;
-}
-
 void SymbolTable::printSymbolTable() {
-    vector<Symbol*>::iterator it = symbols.begin();
+    it = symbols.begin();
     while (it != symbols.end()) {
-        string name = (*it)->name;
-        string scope = (*it)->scope;
-        string visibility = (*it)->visibility;
-        std::cout << name << ", " << scope << ", " << visibility << "\n";
+        std::cout << (*it)->name << ", " << (*it)->type << " #\n";
         it++;
     }
 }
@@ -70,12 +21,9 @@ void SymbolTable::printSymbolTable() {
 Token Parser::consume(TokenType expected_type) {
     Token t = lexer.GetToken();
     // t.Print();
-    // cout << "Expected Type: " << expected_type << "                   " << t.lexeme << "       " << t.token_type << "\n";
-    // cout << "printed the statement\n";
     if (t.token_type != expected_type) {
         syntax_error();
     }
-    // cout << "\n\n";
     return t;
 }
 
@@ -84,225 +32,98 @@ void Parser::syntax_error() {
     exit(1);
 }
 
-void Parser::printStatements(vector<Statement*> stmt_list) {
-    for (vector<Statement*>::iterator it = stmt_list.begin(); it != stmt_list.end(); it++) {
-        std::cout << (*it)->lhs << " = " << (*it)->rhs << "\n";
-    }
-}
-
-void SymbolTable::applyScopingRule(Statement* stmt, string scope) {
-    string lhs_name = stmt->lhs;
-    string rhs_name = stmt->rhs;
-    bool lhs_undefined = true;
-    bool rhs_undefined = true;
-
-    // std::cout << lhs_name << " = " << rhs_name << "\n";
-    // printSymbolTable();
-
-    for (auto it = symbols.rbegin(); it != symbols.rend(); it++) {
-        string name = (*it)->name;
-        string visibility = (*it)->visibility;
-        if (visibility == "::" || visibility == "public") {
-            if (stmt->lhs == name || stmt->rhs == name) {
-                if (stmt->lhs == name) {
-                    lhs_undefined = false;
-                    // std::cout << "equals " << name << "\n";
-                    if (visibility == "::") {
-                        stmt->lhs = (*it)->scope + name;
-
-                    } else {
-                        stmt->lhs = (*it)->scope + "." + name;
-                    }
-                }
-                if (stmt->rhs == name) {
-                    // std::cout << "equals " << name << "\n";
-                    rhs_undefined = false;
-                    if (visibility == "::") {
-                        stmt->rhs = (*it)->scope + name;
-
-                    } else {
-                        stmt->rhs = (*it)->scope + "." + name;
-                    }
-                }
-            }
-        } else if (visibility == "?") {
-            if (stmt->lhs == name && scope == (*it)->scope) {
-                lhs_undefined = false;
-                // std::cout << "equals " << name << "\n";
-                stmt->lhs = (*it)->scope + "." + name;
-            }
-            if (stmt->rhs == name && scope == (*it)->scope) {
-                // std::cout << "equals " << name << "\n";
-                rhs_undefined = false;
-                stmt->rhs = (*it)->scope + "." + name;
-            }
-        }
-    }
-
-    if (lhs_undefined) {
-        stmt->lhs = "?." + lhs_name;
-    }
-    if (rhs_undefined) {
-        stmt->rhs = "?." + rhs_name;
-    }
-}
-
-void SymbolTable::applyScopingRules(vector<Statement*> stmt_list, string scope) {
-    vector<Statement*>::iterator it = stmt_list.begin();
-    while (it != stmt_list.end()) {
-        applyScopingRule(*it, scope);
-        it++;
-    }
-}
-
 void Parser::parse_program() {
-    addSymbols(parse_global_vars(), "::", "::");
-    vector<Statement*> stmt_list = parse_body();
-    // printStatements(this->stmt_list);
+    parse_global_vars();
+    parse_body();
+    // symbolTable.printSymbolTable();
 }
 
-set<string> Parser::parse_global_vars() {
-    set<string> global_vars;
-
+void Parser::parse_global_vars() {
     // Check if it contains epsilon
     Token t = lexer.GetToken();
     lexer.UngetToken(t);
     if (t.token_type == ID) {
         parse_var_decl_list();
     }
-    return global_vars;
 }
 
-set<string> Parser::parse_var_decl_list() {
-    // cout << " Inside var_decl_list\n";
-    set<string> var_decl_list;
-    set<string> var_decl = parse_var_decl();
-    for (set<string>::iterator it = var_decl.begin(); it != var_decl.end(); it++) {
-        var_decl_list.insert(*it);
-    }
+void Parser::parse_var_decl_list() {
+    parse_var_decl();
 
     Token t = lexer.GetToken();
     lexer.UngetToken(t);
     if (t.token_type == ID) {
-        var_decl = parse_var_decl_list();
-        for (set<string>::iterator it = var_decl.begin(); it != var_decl.end(); it++) {
-            var_decl_list.insert(*it);
-        }
+        parse_var_decl_list();
     }
-    // cout << " Exiting var_decl_list\n";
-    return var_decl_list;
 }
 
-set<string> Parser::parse_var_decl() {
-    // cout << " Inside var_decl\n";
-    set<string> var_decl;
-    set<string> var_list = parse_var_list();
-    for (set<string>::iterator it = var_list.begin(); it != var_list.end(); it++) {
-        var_decl.insert(*it);
-    }
+void Parser::parse_var_decl() {
+    vector<Token> var_decl = parse_var_list();
     consume(COLON);
-    parse_type_name();
+    string type = parse_type_name();
+
+    for (vector<Token>::iterator it = var_decl.begin(); it != var_decl.end(); it++) {
+        Symbol* symbol = new Symbol();
+        symbol->name = (*it).lexeme;
+        symbol->type = type;
+        symbolTable.addSymbol(symbol);
+    }
+
     consume(SEMICOLON);
-    return var_decl;
 }
 
 // TODO: Explicit variables are declared in an var_list of a var_decl
-set<string> Parser::parse_var_list() {
-    // cout << " Inside parse_var_list\n";
-    set<string> var_list;
-    var_list.insert(consume(ID).lexeme);
+vector<Token> Parser::parse_var_list() {
+    vector<Token> var_list;
+    var_list.push_back(consume(ID));
 
     Token t = lexer.GetToken();
     if (t.token_type == COMMA) {
-        // t.Print();
-        set<string> var_list2 = parse_var_list();
-        for (set<string>::iterator it = var_list2.begin(); it != var_list2.end(); it++) {
-            var_list.insert(*it);
+        vector<Token> var_list2 = parse_var_list();
+        for (vector<Token>::iterator it = var_list2.begin(); it != var_list2.end(); it++) {
+            var_list.push_back(*it);
         }
     } else {
         lexer.UngetToken(t);
     }
-    // cout << " Exiting parse_var_list\n";
+
     return var_list;
 }
 
 // TODO: check for INT, REAL, BOOL
-void Parser::parse_type_name() {
+string Parser::parse_type_name() {
     Token t = lexer.GetToken();
-    lexer.UngetToken(t);
 
     if (t.token_type == INT) {
-        consume(INT);
+        return "int";
     } else if (t.token_type == REAL) {
-        consume(REAL);
+        return "real";
     } else if (t.token_type == BOOL) {
-        consume(BOOL);
+        return "bool";
     } else {
         syntax_error();
     }
+
+    return "";
 }
 
-vector<Statement*> Parser::parse_body() {
-    // Token t = consume(ID);
-    // string scope = t.lexeme;
-    // consume(LBRACE);
-    // addSymbols(parse_public_vars(), scope, "public");
-    // addSymbols(parse_private_vars(), scope, "?");
-
-    // vector<Statement*> stmt_list = parse_stmt_list();
-    // symbolTable.applyScopingRules(stmt_list, scope);  // apply scoping rules
-
-    // consume(RBRACE);
-    // symbolTable.deleteSymbolsFromScope(scope);
-
-    // return stmt_list;
-    // cout << " Inside parse body";
+void Parser::parse_body() {
     consume(LBRACE);
-    vector<Statement*> stmt_list = parse_stmt_list();
+    parse_stmt_list();
     consume(RBRACE);
-    return stmt_list;
 }
 
-vector<Statement*> Parser::parse_stmt_list() {
-    vector<Statement*> stmt_list;
-    // Statement* stmt = parse_stmt();
-    // if (stmt != NULL) {
-    //     stmt_list.push_back(stmt);
-    //     this->stmt_list.push_back(stmt);
-    // }
-
-    // Token t = lexer.GetToken();
-    // if (t.token_type == ID) {
-    //     lexer.UngetToken(t);
-    //     vector<Statement*> stmt_list2 = parse_stmt_list();
-    //     for (int i = 0; i < stmt_list2.size(); i++) {
-    //         stmt_list.push_back(stmt_list2.at(i));
-    //     }
-    // } else {
-    //     lexer.UngetToken(t);
-    // }
-    Statement* stmt = parse_stmt();
-    if (stmt != NULL) {
-        stmt_list.push_back(stmt);
-        this->stmt_list.push_back(stmt);
-    }
+void Parser::parse_stmt_list() {
+    parse_stmt();
 
     Token t = lexer.GetToken();
     lexer.UngetToken(t);
     if (t.token_type == IF || t.token_type == WHILE || t.token_type == SWITCH || t.token_type == WHILE || t.token_type == ID) {
-        vector<Statement*> stmt_list2 = parse_stmt_list();
-        for (int i = 0; i < stmt_list2.size(); i++) {
-            stmt_list.push_back(stmt_list2.at(i));
-        }
+        parse_stmt_list();
     }
-
-    return stmt_list;
 }
 
-Statement* Parser::parse_stmt() {
-    // cout << " Inside parse_stmt\n";
-    Statement* stmt = new Statement();
-
+void Parser::parse_stmt() {
     Token u = lexer.GetToken();
     lexer.UngetToken(u);
 
@@ -317,41 +138,17 @@ Statement* Parser::parse_stmt() {
     } else {
         syntax_error();
     }
-
-    // Token t = lexer.GetToken();
-    // if (t.token_type == EQUAL) {
-    //     // t.Print();
-    //     stmt->lhs = u.lexeme;
-    //     stmt->rhs = consume(ID).lexeme;
-    //     consume(SEMICOLON);
-    // } else if (t.token_type == LBRACE) {
-    //     lexer.UngetToken(t);
-    //     lexer.UngetToken(u);
-    //     parse_body();
-    //     return NULL;  // If there is a scope, there is no statement to return
-    // } else {
-    //     syntax_error();
-    // }
-    // cout << " Exiting parse_stmt\n";
-    return stmt;
 }
 
-Statement* Parser::parse_assignment_stmt() {
-    Statement* stmt = new Statement();
-
+void Parser::parse_assignment_stmt() {
     Token t = consume(ID);
-    stmt->lhs = t.lexeme;
     consume(EQUAL);
-    parse_expression();
+    vector<Token> expression = parse_expression();
     consume(SEMICOLON);
-
-    return stmt;
 }
 
 // TODO: Fix
 vector<Token> Parser::parse_expression() {
-    // cout << " Inside parse_expression\n";
-
     // string expression;
     Token t = lexer.GetToken();
     lexer.UngetToken(t);
@@ -393,48 +190,18 @@ Token Parser::parse_unary_operator() {
 }
 
 Token Parser::parse_binary_operator() {
-    // cout << " Inside binary_operator\n";
     Token t = lexer.GetToken();
     lexer.UngetToken(t);
-    // t.Print();
+
     if (t.token_type == PLUS || t.token_type == MINUS || t.token_type == MULT || t.token_type == DIV || t.token_type == GREATER || t.token_type == LESS || t.token_type == GTEQ || t.token_type == LTEQ || t.token_type == EQUAL || t.token_type == NOTEQUAL) {
-        // cout << " Exiting binary_operator\n";
-        // t.Print();
         consume(t.token_type);
     } else {
         syntax_error();
     }
-
-    // if (t.token_type == PLUS) {
-    //     return "+";
-    // } else if (t.token_type == MINUS) {
-    //     return "-";
-    // } else if (t.token_type == MULT) {
-    //     return "*";
-    // } else if (t.token_type == DIV) {
-    //     return "/";
-    // } else if (t.token_type == GREATER) {
-    //     return ">";
-    // } else if (t.token_type == LESS) {
-    //     return "<";
-    // } else if (t.token_type == GTEQ) {
-    //     return ">=";
-    // } else if (t.token_type == LTEQ) {
-    //     return "<=";
-    // } else if (t.token_type == EQUAL) {
-    //     return "=";
-    // } else if (t.token_type == NOTEQUAL) {
-    //     return "<>";
-    // } else {
-    //     syntax_error();
-    // }
-
-    // cout << " Exiting binary_operator\n";
     return t;
 }
 
 Token Parser::parse_primary() {
-    // cout << " Inside parse_primary\n";
     Token t = lexer.GetToken();
     lexer.UngetToken(t);
 
@@ -453,21 +220,13 @@ Token Parser::parse_primary() {
     }
 
     return t;
-    // cout << " Exiting parse_primary\n";
 }
 
 // TODO
 void Parser::parse_if_stmt() {
-    // cout << " Inside if_stmt\n";
     consume(IF);
     consume(LPAREN);
     vector<Token> tokens = parse_expression();
-
-    // cout << "\nSTARTING TOKEN LIST\n";
-    // for (Token i : tokens) {
-    //     i.Print();
-    // }
-    // cout << "\nENDING TOKEN LIST\n";
 
     TokenType type = tokens.at(0).token_type;
     if (type != GREATER && type != LESS && type != GTEQ && type != LTEQ && type != EQUAL && type != NOTEQUAL) {
@@ -493,11 +252,9 @@ void Parser::parse_if_stmt() {
 
     consume(RPAREN);
     parse_body();
-    // cout << " Exiting if_stmt\n";
 }
 
 void Parser::parse_while_stmt() {
-    // cout << " Inside while_stmt\n";
     consume(WHILE);
     consume(LPAREN);
     vector<Token> tokens = parse_expression();
@@ -509,7 +266,6 @@ void Parser::parse_while_stmt() {
 
     consume(RPAREN);
     parse_body();
-    // cout << " Exiting while_stmt\n";
 }
 
 void Parser::parse_switch_stmt() {
@@ -518,7 +274,6 @@ void Parser::parse_switch_stmt() {
     parse_expression();
     consume(RPAREN);
     consume(LBRACE);
-    // cout << "       --- I AM HERE ---";
     parse_case_list();
     consume(RBRACE);
 }
@@ -539,49 +294,6 @@ void Parser::parse_case() {
     consume(COLON);
     parse_body();
 }
-
-// set<string> Parser::parse_public_vars() {
-//     set<string> public_vars;
-//     // Check if it contains epsilon
-//     Token t = lexer.GetToken();
-//     if (t.token_type != PUBLIC) {
-//         lexer.UngetToken(t);
-//         return public_vars;
-//     } else {
-//         // It does not contain epsilon
-//         lexer.UngetToken(t);
-//     }
-
-//    // If it doesn't contain epsilon
-//    consume(PUBLIC);
-//    consume(COLON);
-//    public_vars = parse_var_list();
-//    consume(SEMICOLON);
-
-//    return public_vars;
-//}
-
-// set<string> Parser::parse_private_vars() {
-//     set<string> private_vars;
-
-//    // Check if it contains epsilon
-//    Token t = lexer.GetToken();
-//    if (t.token_type != PRIVATE) {
-//        lexer.UngetToken(t);
-//        return private_vars;
-//    } else {
-//        // It does not contain epsilon
-//        lexer.UngetToken(t);
-//    }
-
-//    // If it doesn't contain epsilon
-//    consume(PRIVATE);
-//    consume(COLON);
-//    private_vars = parse_var_list();
-//    consume(SEMICOLON);
-
-//    return private_vars;
-//}
 
 int main() {
     Parser parser;
--- parser.h
+++ parser.h
@@ -7,30 +7,20 @@ using std::set;
 using std::string;
 using std::vector;
 
-struct Statement {
-    string lhs;
-    string rhs;
-};
-
 struct Symbol {
     string name;
-    string scope;
-    string visibility;
+    string type;
 };
 
 class SymbolTable {
    public:
-    vector<Symbol *> symbols;
     Symbol *symbolExists(string symbolName);
-    void applyScopingRules(vector<Statement *>, string scope);
     void addSymbol(Symbol *symbol);
-    void deleteSymbolsFromScope(string scope);
     void printSymbolTable();
 
    private:
-    int indexOf(string symbol);
-    void applyScopingRule(Statement *stmt, string scope);
-    void deleteSymbol(Symbol *symbol);
+    vector<Symbol *> symbols;
+    vector<Symbol *>::iterator it;
 };
 
 class Parser {
@@ -38,19 +28,18 @@ class Parser {
     LexicalAnalyzer lexer;
     SymbolTable symbolTable;
     void parse_program();
-    vector<Statement *> stmt_list;
 
    private:
     // Parse Functions
-    set<string> parse_global_vars();
-    set<string> parse_var_decl_list();
-    set<string> parse_var_decl();
-    set<string> parse_var_list();
-    void parse_type_name();
-    vector<Statement *> parse_body();
-    vector<Statement *> parse_stmt_list();
-    Statement *parse_stmt();
-    Statement *parse_assignment_stmt();
+    void parse_global_vars();
+    void parse_var_decl_list();
+    void parse_var_decl();
+    vector<Token> parse_var_list();
+    string parse_type_name();
+    void parse_body();
+    void parse_stmt_list();
+    void parse_stmt();
+    void parse_assignment_stmt();
     vector<Token> parse_expression();
     Token parse_unary_operator();
     Token parse_binary_operator();
@@ -64,6 +53,4 @@ class Parser {
     // Helper Functions
     void syntax_error();
     Token consume(TokenType expected_type);
-    void printStatements(vector<Statement *> stmt_list);
-    void addSymbols(set<string> symbols, string scope, string visibility);
 };
